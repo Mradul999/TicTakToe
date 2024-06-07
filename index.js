7bec62454373bcd5473dfb3c41d4f6a65d0f4fe2
Mradul999 <demorgans007@gmail.com>
2024-06-07 11:36:57 +0530

    commit

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ boxes.forEach((box) => {
         fillCross(box);
       }
       winnerPlayer();
+    //   gameTied();
     }
   });
 });
+// let count = 0;
 
 //function to fill circle in the box
 function fillCircle(boxValue) {
@@ -36,15 +38,22 @@ const newGameButton = document.querySelector(".new-game");
 // function which reset the game
 function startNewGame() {
   circleCross.innerText = "X";
+  currentPlayeOrWinner.innerText = "Current Player-";
+
   boxes.forEach((box) => {
     box.innerText = "";
     box.style.backgroundColor = "";
+  });
+
+  boxes.forEach((box) => {
+    box.style.pointerEvents = "all";
   });
 }
 
 //to start a new game button event listener
 newGameButton.addEventListener("click", function () {
   startNewGame();
+  newGameButton.classList.remove("active");
 });
 
 //function for winner
@@ -57,6 +66,8 @@ const box6 = document.querySelector(".box6");
 const box7 = document.querySelector(".box7");
 const box8 = document.querySelector(".box8");
 const box9 = document.querySelector(".box9");
+
+let win;
 
 function winnerPlayer() {
   const boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9];
@@ -84,9 +95,30 @@ function winnerPlayer() {
       currentPlayeOrWinner.innerText = "Winner-";
       circleCross.innerText = boxes[pattern[0]].innerText;
 
+      boxes.forEach((box) => {
+        box.style.pointerEvents = "none";
+      });
+      newGameButton.classList.add("active");
+
+
+
       return;
     }
+    let count=0;
+    boxes.forEach((counter) => {
+        if (counter.innerText !== "") {
+          count++;
+        }
+    });
+
+    if(count===9){
+        currentPlayeOrWinner.innerText = "Game Tied!!";
+      circleCross.innerText = "";
+
+    }
+
+
   });
 }
 
-winnerPlayer();
+
